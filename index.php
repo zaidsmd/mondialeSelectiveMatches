2939631b42f49088f1d5c6b2f97f84fb038a41ff
zaidsmd <zaidsmd111@gmail.com>
2023-01-18 23:51:05 +0100

    calc script

--- index.php
+++ index.php
@@ -1,117 +1,34 @@
 <?php
-$groupA = ["name" => "Group A",
-    "countries"=>[
-        "Qatar" => ["name" => "Qatar", "flag" => "QA", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Ecuador" => ["name" => "Ecuador", "flag" => "EC", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Senegal" => ["name" => "Senegal", "flag" => "SN", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Netherlands" => ["name" => "Netherlands", "flag" => "NL", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0]
-    ]
-];
-$groupB = ["name" => "Group B",
-    "countries"=>[
-        "Georgia" => ["name" => "Georgia", "flag" => "GE", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "United States" => ["name" => "United States", "flag" => "US", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Iran" => ["name" => "Iran", "flag" => "IR", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Honduras" => ["name" => "Honduras", "flag" => "HN", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0]
-    ]
-];
-$groupC = ["name" => "Group C",
-   "countries"=>[
-       "Saudi Arabia" => ["name" => "Saudi Arabia", "flag" => "SA", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-       "Poland" => ["name" => "Poland", "flag" => "PL", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-       "Argentina" => ["name" => "Argentina", "flag" => "AR", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-       "Mexico" => ["name" => "Mexico", "flag" => "MX", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0]
-   ]
-];
-$groupD = ["name" => "Group D",
-    "countries"=>[
-        "France" => ["name" => "France", "flag" => "FR", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Australia" => ["name" => "Australia", "flag" => "AU", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Denmark" => ["name" => "Denmark", "flag" => "DK", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Tunisia" => ["name" => "Tunisia", "flag" => "TN", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0]
-    ]
-];
-$groupE = ["name" => "Group E",
-   "countries"=>[
-       "Spain" => ["name" => "Spain", "flag" => "ES", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-       "Germany" => ["name" => "Germany", "flag" => "DE", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-       "Japan" => ["name" => "Japan", "flag" => "JP", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-       "Costa Rica" => ["name" => "Costa Rica", "flag" => "CR", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0]
-   ]
-];
-$groupF = ["name" => "Group F",
-    "countries"=>[
-        "Belgium" => ["name" => "Belgium", "flag" => "BE", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Croatia" => ["name" => "Croatia", "flag" => "HR", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Morocco" => ["name" => "Morocco", "flag" => "MA", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Canada" => ["name" => "Canada", "flag" => "CA", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0]
-    ]
-];
-$groupG = ["name" => "Group G",
-    "countries"=>[
-        "Brazil" => ["name" => "Brazil", "flag" => "BR", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Cameroon" => ["name" => "Cameroon", "flag" => "CM", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Switzerland" => ["name" => "Switzerland", "flag" => "CH", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-        "Serbia" => ["name" => "Serbia", "flag" => "MX", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0]
-    ]
-];
-$groupH = ["name" => "Group H",
-   "countries"=>[
-       "Portugal" => ["name" => "Portugal", "flag" => "PT", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-       "Poland" => ["name" => "Poland", "flag" => "PL", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-       "Argentina" => ["name" => "Iran", "flag" => "IR", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0],
-       "Mexico" => ["name" => "Mexico", "flag" => "MX", "mp" => 0, "w" => 0, "d" => 0,"l"=>0,"gf"=>0,"ga"=>0,"gd"=>0,"pts"=>0]
-   ]
-];
-$groups = [$groupA,$groupB,$groupC,$groupD,$groupE,$groupF,$groupG,$groupH];
+include "PHP/data.php";
+include "PHP/head.php";
+if (isset($_POST["group"])){
+    $group = $_POST["group"];
+    $keys = array_keys($$group["countries"]);
+    $Match1 = [$$group["countries"][$keys[0]]["name"]=>(int)$_POST[$$group["countries"][$keys[0]]["name"]."M1"],
+        $$group["countries"][$keys[1]]["name"]=>(int)$_POST[$$group["countries"][$keys[1]]["name"]."M1"]];
+    $Match2 = [$$group["countries"][$keys[2]]["name"]=>(int)$_POST[$$group["countries"][$keys[2]]["name"]."M1"],
+        $$group["countries"][$keys[3]]["name"]=>(int)$_POST[$$group["countries"][$keys[3]]["name"]."M1"]];
+    $Match3 = [$$group["countries"][$keys[2]]["name"]=>(int)$_POST[$$group["countries"][$keys[2]]["name"]."M2"],
+        $$group["countries"][$keys[0]]["name"]=>(int)$_POST[$$group["countries"][$keys[0]]["name"]."M2"]];
+    $Match4 = [$$group["countries"][$keys[1]]["name"]=>(int)$_POST[$$group["countries"][$keys[1]]["name"]."M2"],
+        $$group["countries"][$keys[3]]["name"]=>(int)$_POST[$$group["countries"][$keys[3]]["name"]."M2"]];
+    $Match5 = [$$group["countries"][$keys[0]]["name"]=>(int)$_POST[$$group["countries"][$keys[0]]["name"]."M3"],
+        $$group["countries"][$keys[3]]["name"]=>(int)$_POST[$$group["countries"][$keys[3]]["name"]."M3"]];
+    $Match6 = [$$group["countries"][$keys[2]]["name"]=>(int)$_POST[$$group["countries"][$keys[2]]["name"]."M3"],
+        $$group["countries"][$keys[1]]["name"]=>(int)$_POST[$$group["countries"][$keys[1]]["name"]."M3"]];
+
+    echo $$group["countries"][$keys[1]]["name"];
+    echo (int )$_POST[$$group["countries"][$keys[0]]["name"]."M1"];
+}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>World Cup Matcher</title>
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary navbar-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img
-                src="https://seeklogo.com/images/F/fifa-world-cup-qatar-2022-logo-32157477CB-seeklogo.com.png"
-                alt="logo"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Points Simulator</a>
-                <a class="nav-link" href="#">Group Stage</a>
-                <a class="nav-link" href="#">Knockout Stage</a>
-            </div>
-        </div>
-    </div>
-</nav>
 <section>
     <div class="line">
         Groups
     </div>
     <div class="container d-flex flex-wrap col-12">
         <?php
-        foreach ($groups as $group){echo "<div class='card col-3'>
-            <div class='card-body'>
-                <h5 class='card-title'>${group["name"]}</h5>
-                <ul>";
-            foreach ($group["countries"] as $country){
-                echo "
-                    <li><img src='https://flagsapi.com/${country["flag"]}/shiny/32.png' alt='${country["name"]}'><span>${country["name"]}</span></li>";}
-            echo "</ul>
-                <a class='btn btn-primary' data-target='${group["name"]}'>Simulate</a>
-            </div>
-        </div>";}?>
+        include "PHP/cards.php"
+        ?>
     </div>
 </section>
 <section>
@@ -119,172 +36,9 @@ $groups = [$groupA,$groupB,$groupC,$groupD,$groupE,$groupF,$groupG,$groupH];
         Simulation
     </div>
     <?php
-    foreach ($groups as $group){
-        $keys = array_keys($group["countries"]);
-        echo "<form action='<?php echo htmlspecialchars(\$_SERVER[\"PHP_SELF\"]) ?>' id='${group["name"]}' class='hide' method='post'>
-    <div class='container d-flex flex-wrap col-12'>
-        <div class='card col-5'>
-            <div class='card-body'>
-                <h5 class='card-title'>Match 1</h5>
-                <div class='form'>
-                    <label for='" . $group["countries"][$keys[0]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[0]]["flag"] . "/shiny/64.png'>
-                    </label>
-                    <input type='number' name=" . $group["countries"][$keys[0]]["name"] . "M1' id='" . $group["countries"][$keys[0]]["name"] . "'>
-                    <div>
-                        <div class='circle'></div>
-                        <div class='circle'></div>
-                    </div>
-                    <input type='number' name='" . $group["countries"][$keys[1]]["name"] . "M1' id='" . $group["countries"][$keys[1]]["name"] . "'>
-                    <label for='" . $group["countries"][$keys[1]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[1]]["flag"] . "/shiny/64.png'>
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class='card col-5'>
-            <div class='card-body'>
-                <h5 class='card-title'>Match 2</h5>
-                <div class='form'>
-                    <label for='" . $group["countries"][$keys[2]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[2]]["flag"] . "/shiny/64.png'>
-                    </label>
-                    <input type='number' name=" . $group["countries"][$keys[2]]["name"] . "M1' id='" . $group["countries"][$keys[2]]["name"] . "'>
-                    <div>
-                        <div class='circle'></div>
-                        <div class='circle'></div>
-                    </div>
-                    <input type='number' name='" . $group["countries"][$keys[3]]["name"] . "M1' id='" . $group["countries"][$keys[3]]["name"] . "'>
-                    <label for='" . $group["countries"][$keys[3]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[3]]["flag"] . "/shiny/64.png'>
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class='card col-5'>
-            <div class='card-body'>
-                <h5 class='card-title'>Match 3</h5>
-                <div class='form'>
-                    <label for='" . $group["countries"][$keys[2]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[2]]["flag"] . "/shiny/64.png'>
-                    </label>
-                    <input type='number' name=" . $group["countries"][$keys[2]]["name"] . "M2' id='" . $group["countries"][$keys[2]]["name"] . "'>
-                    <div>
-                        <div class='circle'></div>
-                        <div class='circle'></div>
-                    </div>
-                    <input type='number' name='" . $group["countries"][$keys[0]]["name"] . "M2' id='" . $group["countries"][$keys[0]]["name"] . "'>
-                    <label for='" . $group["countries"][$keys[0]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[0]]["flag"] . "/shiny/64.png'>
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class='card col-5'>
-            <div class='card-body'>
-                <h5 class='card-title'>Match 4</h5>
-                <div class='form'>
-                    <label for='" . $group["countries"][$keys[3]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[3]]["flag"] . "/shiny/64.png'>
-                    </label>
-                    <input type='number' name=" . $group["countries"][$keys[3]]["name"] . "M2' id='" . $group["countries"][$keys[3]]["name"] . "'>
-                    <div>
-                        <div class='circle'></div>
-                        <div class='circle'></div>
-                    </div>
-                    <input type='number' name='" . $group["countries"][$keys[1]]["name"] . "M2' id='" . $group["countries"][$keys[1]]["name"] . "'>
-                    <label for='" . $group["countries"][$keys[1]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[1]]["flag"] . "/shiny/64.png'>
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class='card col-5'>
-            <div class='card-body'>
-                <h5 class='card-title'>Match 5</h5>
-                <div class='form'>
-                    <label for='" . $group["countries"][$keys[0]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[0]]["flag"] . "/shiny/64.png'>
-                    </label>
-                    <input type='number' name=" . $group["countries"][$keys[0]]["name"] . "M3' id='" . $group["countries"][$keys[0]]["name"] . "'>
-                    <div>
-                        <div class='circle'></div>
-                        <div class='circle'></div>
-                    </div>
-                    <input type='number' name='" . $group["countries"][$keys[3]]["name"] . "M3' id='" . $group["countries"][$keys[3]]["name"] . "'>
-                    <label for='" . $group["countries"][$keys[3]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[3]]["flag"] . "/shiny/64.png'>
-                    </label>
-                </div>
-            </div>
-        </div>
-        <div class='card col-5'>
-            <div class='card-body'>
-                <h5 class='card-title'>Match 6</h5>
-                <div class='form'>
-                    <label for='" . $group["countries"][$keys[2]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[2]]["flag"] . "/shiny/64.png'>
-                    </label>
-                    <input type='number' name=" . $group["countries"][$keys[2]]["name"] . "M3' id='" . $group["countries"][$keys[2]]["name"] . "'>
-                    <div>
-                        <div class='circle'></div>
-                        <div class='circle'></div>
-                    </div>
-                    <input type='number' name='" . $group["countries"][$keys[1]]["name"] . "M3' id='" . $group["countries"][$keys[1]]["name"] . "'>
-                    <label for='" . $group["countries"][$keys[1]]["name"] . "'>
-                        <img src='https://flagsapi.com/" . $group["countries"][$keys[1]]["flag"] . "/shiny/64.png'>
-                    </label>
-                </div>
-            </div>
-        </div>
-    </div>
-    <input type='submit' value='Simulate'>
-    <table class='col-10'>
-        <tr>
-            <th>#</th>
-            <th>Team</th>
-            <th>MP</th>
-            <th>W</th>
-            <th>D</th>
-            <th>L</th>
-            <th>GF</th>
-            <th>GA</th>
-            <th>GD</th>
-            <th>Pts</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>Qatar</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Ecuador</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-        </tr>
-    </table>
-</form>";
-    }
+    include "PHP/matches.php"
     ?>
-
 </section>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+<?php
+include "PHP/foot.php";
 
---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.card-body .btn-primary').forEach(e=>{
+    e.addEventListener("click",()=>{
+        document.querySelectorAll('form').forEach(form=>{
+            form.classList.add('hide');
+        })
+        document.querySelector("#"+e.dataset.target).classList.remove('hide');
+    })
+});
+document.querySelector('#GroupA').classList.remove('hide');
